8838d89db34bbb308d45fccb7d845c369b609ecf
loztprophet <allan@alcacc.com>
2020-05-05 12:49:49 -0400

    Add files via upload

---
+++ StayAwake.py
@@ -0,0 +1,35 @@
+__author__ = "Tshark Online"
+__license__ = "GPL"
+__version__ = "1.0.0"
+__maintainer__ = "Tshark"
+__status__ = "Production"
+
+from pyautogui import press
+from time import localtime, strftime, sleep
+from ctypes import Structure, windll, c_uint, sizeof, byref
+
+class LASTINPUTINFO(Structure):
+    _fields_ = [
+        ('cbSize', c_uint),
+        ('dwTime', c_uint),
+    ]
+
+def get_idle_duration():
+    lastInputInfo = LASTINPUTINFO()
+    lastInputInfo.cbSize = sizeof(lastInputInfo)
+    windll.user32.GetLastInputInfo(byref(lastInputInfo))
+    millis = windll.kernel32.GetTickCount() - lastInputInfo.dwTime
+    return millis / 1000.0
+
+def cur_time():
+    return strftime("%Y-%m-%d %H:%M:%S", localtime())
+
+print(f"{cur_time()}:Starting...")
+while(True):
+    idle_time= get_idle_duration()
+    if(idle_time>300):
+        press("shift")
+        print(f"{cur_time()}:Shift pressed")
+    else:
+        print(f"{cur_time()}:Idle_time {idle_time}. Sleeping for 100s") 
+        sleep(100)
