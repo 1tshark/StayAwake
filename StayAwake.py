4f35c673f76ea6ca787eb2914d3422709565eb19
loztprophet <allan@alcacc.com>
2020-05-05 21:18:30 -0400

    Improvements.

--- StayAwake.py
+++ StayAwake.py
@@ -1,11 +1,9 @@
 __author__ = "Tshark Online"
 __license__ = "GPL"
-__version__ = "1.0.0"
-__maintainer__ = "Tshark"
-__status__ = "Production"
+__version__ = "2.0.0"
 
 from pyautogui import press
-from time import localtime, strftime, sleep
+from time import localtime, strftime, sleep, gmtime
 from ctypes import Structure, windll, c_uint, sizeof, byref
 
 class LASTINPUTINFO(Structure):
@@ -24,6 +22,9 @@ def get_idle_duration():
 def cur_time():
     return strftime("%Y-%m-%d %H:%M:%S", localtime())
 
+def convert(seconds): 
+    return strftime("%H:%M:%S", gmtime(seconds)) 
+
 print(f"{cur_time()}:Starting...")
 inactive = 0
 while(True):
@@ -31,13 +32,13 @@ while(True):
     if(idle_time<1): inactive = 0    
     if(idle_time>300):
         if(inactive>3600):
-            print(f"You been inactive for {(inactive/60)/60}hr. Good Bye!")
+            print(f"You been inactive for {convert(inactive)}. Good Bye!")
             break
         else:
             inactive+=idle_time
             press("shift")
-            print(f"{cur_time()}:Shift pressed. Inactive for {(inactive/60)}m")
+            print(f"{cur_time()}:Shift pressed. Inactive for {convert(inactive)}")
             sleep(2)    
     else:
         print(f"{cur_time()}: idle time:{idle_time}") 
-        sleep(30)
+        sleep(60)
